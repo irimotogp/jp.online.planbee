10a318cd13afde12e660d02b2d0e08101bbc10d1
Krishna <everyfor0324@gmail.com>
2022-05-12 11:12:51 +0900

    0512 1112

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -20,7 +20,7 @@ $appCss = mix('dist/css/app.css');
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <title>{{ config('app.name') }}</title>
-  <link rel="stylesheet" href="{{ (str_starts_with($appCss, '//') ? 'http:' : '').$appCss }}">
+  <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}">
 </head>
 <body>
   <div id="app"></div>
@@ -29,6 +29,6 @@ $appCss = mix('dist/css/app.css');
     window.config = @json($config);
   </script>
 
-  <script src="{{ (str_starts_with($appJs, '//') ? 'http:' : '').$appJs }}"></script>
+  <script src="{{ mix('dist/js/app.js') }}"></script>
 </body>
 </html>
