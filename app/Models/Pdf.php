22e5cdf61973ad4151e4c92a3b56ceda6bc3b450
Everyfor0324 <everyfor0324@gmail.com>
2022-10-25 20:42:22 +0900

    10252042

--- app/Models/Pdf.php
+++ app/Models/Pdf.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class pdf extends Model
+class Pdf extends Model
 {
     use \Backpack\CRUD\app\Models\Traits\CrudTrait;
     use HasFactory;
